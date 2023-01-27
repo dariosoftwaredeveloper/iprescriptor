0ecfa64db71d61153cf13ce5215fcd275e1a91a4
dariosoftwaredeveloper <123776485+dariosoftwaredeveloper@users.noreply.github.com>
2023-01-27 16:33:17 -0500

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,10 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
+
+    
+    
+#SECRET_KEY: Es una clave secreta utilizada para proteger la aplicación. Se establece utilizando una variable de entorno llamada 'SECRET_KEY', o utilizando un valor predeterminado 'my_secret_key' si no se encuentra la variable de entorno.
+#SQLALCHEMY_DATABASE_URI: Es la URI de la base de datos que se utilizará para almacenar los datos de la aplicación. Se establece utilizando una variable de entorno llamada 'DATABASE_URL', o utilizando un valor predeterminado 'sqlite:///app.db' si no se encuentra la variable de entorno.
+#SQLALCHEMY_TRACK_MODIFICATIONS: Establece si SQLAlchemy debe hacer un seguimiento de los cambios en las entidades. Se establece en False para mejorar el rendimiento.
+#DEBUG: Establece si la aplicación se ejecuta en modo debug. Se establece en True para facilitar el desarrollo.
